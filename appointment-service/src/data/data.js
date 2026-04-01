648655f707bad810326f954b40e5818ffcd02c8c
Pooji0311 <IT22254184@my.sliit.lk>
2026-04-01 12:58:04 +0530

    Update data.js

--- appointment-service/src/data/data.js
+++ appointment-service/src/data/data.js
@@ -1,4 +1,6 @@
 module.exports = [
   { id: 1, patientId: 1, doctorId: 2, date: "2026-04-02", time: "10:30 AM" },
   { id: 2, patientId: 2, doctorId: 1, date: "2026-04-03", time: "02:00 PM" }
+
+  
 ];
